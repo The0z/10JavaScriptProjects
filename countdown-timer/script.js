7baa2d2569c279069260bdfe4c8748538ebf4394
The0z <63734739+The0z@users.noreply.github.com>
2020-11-19 22:11:07 -0700

    added formatted time (leading zero).

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -16,9 +16,9 @@ function countdowntimer(){
     const months = Math.floor(timeLeft/3600/24/30.4167);
     timeLeft = timeLeft - Math.floor(months*3600*24*30.4167);
     const days = Math.floor(timeLeft/3600/24);
-    const hours = Math.floor(timeLeft/3600%24);
-    const minutes = Math.floor(timeLeft/60%60);
-    const seconds = Math.floor(timeLeft%60);
+    const hours = formatTime(Math.floor(timeLeft/3600%24));
+    const minutes = formatTime(Math.floor(timeLeft/60%60));
+    const seconds = formatTime(Math.floor(timeLeft%60));
     
     document.getElementById('months').innerHTML = months;
     document.getElementById('days').innerHTML = days; 
@@ -30,6 +30,13 @@ function countdowntimer(){
 
 function convertTime(time, divider){
     return Math.floor(time/divider);
+}
+
+//a function to format the time to always have two digits
+function formatTime(time){
+    //create a ternary operator - value = (condition) ? true value : false value
+    // using a template literal (`string ${expression} string`)
+    return (time < 10) ? `0${time}` : time;
 }
 
 //Call countdown time every 100 ms.
