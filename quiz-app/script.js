dc7f890728b1ab7e3b77b0f17b4ce482991a4e39
The0z <63734739+The0z@users.noreply.github.com>
2020-11-21 13:33:35 -0700

    Created Questions

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,1 +1,44 @@
 // The JavaScript file!
+// Array of key-value pairs
+const quizData = [
+    {
+        //key is question, value is the actual question descript/ans
+        //curly brace = object, key: value
+        question: 'When was JavaScript Created?',
+        a: 'December 4th, 1995',
+        b: 'December 4th, 1997',
+        c: 'October 12th, 1995',
+        d: 'October 12th, 1996',
+        correct: 'a'
+    }, {
+        question: 'When was YouTube founded?',
+        a: '2004',
+        b: '2005',
+        c: '2006',
+        correct: 'b'
+    }, { 
+        question: 'What is Tesla\'s self-driving called?',
+        a: 'Super Cruise',
+        b: 'Intelligent Drive',
+        c: 'Autopilot',
+        d: 'Motional',
+        e: 'Openpilot',
+        correct: 'c'
+    } ,{ 
+        question: 'What is my ideal car?',
+        a: 'Dodge Demon',
+        b: 'Corvette',
+        c: 'Mercedes-AMG GT R',
+        d: 'KIA Stinger',
+        correct: 'd'
+    } ,{
+        question: 'Who is my favourite character?',
+        a: 'Ainz Ooal Gown',
+        b: 'Batman',
+        c: 'The Mandalorian',
+        d: 'Andrew Ryan',
+        e: 'Doom Slayer',
+        correct: 'e'
+    }
+
+]
